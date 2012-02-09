5217256a5488f9d1d1900690e1d5343a5287ceb2
Julian Vida <git@gyc2001.com>
2012-02-09 23:24:32 +0100

    Indexestilos

--- index.php
+++ index.php
@@ -1,5 +1,73 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es-ES" lang="es-ES">
 
-echo 'Hola Mundo';
+    <head>
+    </head>
 
-?>
+    <style type="text/css">
+        html {
+            height: 100%;
+            background-color:#ddd;
+        }
+
+        body {
+            margin:0px auto;
+            padding: 0;
+            height: 100%;
+            background-color:#ccc;
+            width:90%;
+            position:relative;
+        }
+
+        header {
+            background-color:#aaa;
+            height:80px;
+            position:absolute;
+            width:100%;
+            margin:0px auto;
+        }
+
+        #contenedor {
+            background-color:#ccc;
+            top:80px;
+            height:100%;
+            overflow:hidden;
+            position:relative;
+        }
+
+        #cuerpo {
+           position:relative;
+        }
+
+        #contenido {
+            background-color:#fff;
+            width:200px;
+            height:200px;
+            margin-top:100px;
+            margin-left:100px;
+        }
+        
+
+    </style>
+
+    <body>
+
+        <div id="cuerpo">
+
+            <header></header>
+
+            <div id="contenedor">
+
+                <div id="contenido">
+                </div>
+
+            </div>
+
+        </div>
+
+    </body>
+
+</html>
+
+
+
